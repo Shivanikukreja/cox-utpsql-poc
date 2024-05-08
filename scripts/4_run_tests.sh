fabd53f5346144a67d64e3f0eb4e3e3499bf280f
Shivanikukreja <169030512+Shivanikukreja@users.noreply.github.com>
2024-05-08 16:20:00 +0530

    Archiving artifacts

--- scripts/4_run_tests.sh
+++ scripts/4_run_tests.sh
@@ -16,4 +16,5 @@ utPLSQL-cli/bin/utplsql run ut3_demo/ut3_demo@//127.0.0.1:1521/XE?oracle.net.dis
   -f=ut_sonar_test_reporter         -o=test_results.xml \
   -f=ut_coverage_cobertura_reporter -o=cobertura.xml \
   -f=ut_junit_reporter              -o=junit_test_results.xml \
-  --failure-exit-code=0
+  --failure-exit-code=0 \
+  | tee test_result.html
